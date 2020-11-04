5363c44f20930296562ae13d0fab1c9353fa2193
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-11-03 18:27:38 -0600

    Created get all and get by id for categories

--- categories/request.py
+++ categories/request.py
@@ -24,3 +24,22 @@ def get_all_categories():
             categories.append(category.__dict__)
 
     return json.dumps(categories)
+
+def get_category_by_id(id):
+    with sqlite3.connect('./rare.db') as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.title
+        FROM categories c
+        WHERE c.id = ?
+        """, (id, ))
+
+        data = db_cursor.fetchone()
+        
+        category = Category(data['id'], data['title'])
+
+        return json.dumps(category.__dict__)
