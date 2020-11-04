f2f37de96f46f22311f7ef591ddc2c66578b5257
Mark Young <markyaaronyoung@gmail.com>
2020-11-03 20:25:35 -0600

    update article functions now post updates through postman

--- articles/requests.py
+++ articles/requests.py
@@ -48,3 +48,26 @@ def create_article(new_article):
         id = db_cursor.lastrowid
         new_article['id'] = id
     return json.dumps(new_article)
+
+def update_article(id, new_article):
+    with sqlite3.connect("./rare.db") as conn:
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        UPDATE articles
+            SET
+                title = ?,
+                content = ?,
+                date = ?,
+                user_id= ?,
+                category_id= ?
+        WHERE id = ?
+        """, (new_article['title'], new_article['content'],
+              new_article['date'], new_article['user_id'],
+              new_article['category_id'], id, ))
+        rows_affected = db_cursor.rowcount
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
